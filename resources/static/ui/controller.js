bde17da44151710dd7eb946c676cef4eff8b384d
sudheerpanda <sudheer.panda@imaginea.com>
2017-03-21 18:42:52 +0530

    commit the new js files

--- resources/static/ui/controller.js
+++ resources/static/ui/controller.js
@@ -1,30 +1,11 @@
-(function(angular) {
-  var AppController = function($scope, Item) {
-    Item.query(function(response) {
-      $scope.items = response ? response : [];
-    });
-    
-    $scope.addItem = function(description) {
-      new Item({
-        description: description,
-        checked: false
-      }).$save(function(item) {
-        $scope.items.push(item);
-      });
-      $scope.newItem = "";
-    };
-    
-    $scope.updateItem = function(item) {
-      item.$update();
-    };
-    
-    $scope.deleteItem = function(item) {
-      item.$remove(function() {
-        $scope.items.splice($scope.items.indexOf(item), 1);
-      });
-    };
-  };
-  
-  AppController.$inject = ['$scope', 'Item'];
-  angular.module("myApp.controllers").controller("AppController", AppController);
-}(angular));
+var app = angular.module('myApp', []);
+app.controller('noteController', function($scope, $http) {
+  $http({
+    method : 'GET',
+    url : 'note/get',
+    params:'name=454'
+  }).then(function mySucces(response) {
+      $scope.name = response.message;
+    }, function myError(response) {
+      $scope.name = response.name;
+  });
